d1b17b73ef033817e3383d2bde6218d859a65d64
C4rim-cibersegurata <mendezluciano1999@gmail.com>
2022-10-03 09:33:19 +0200

    Añadimos el ejercicio 2 de la UD02

---
+++ bootcamp_java_UD02_EX2/src/bootcamp_java_UD02_EX2/ComentarioLinea.java
@@ -0,0 +1,12 @@
+package bootcamp_java_UD02_EX2;
+
+public class ComentarioLinea {
+
+	public static void main(String[] args) {
+		//System.out.println("Hola"): 
+		System.out.println("Adios"); 
+		System.out.println("FIN DE PROGRAMA");
+
+	}
+
+}
